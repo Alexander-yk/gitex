8d980022ce9438f46b3662b2fdd891f264e619a8
yk <1607561875@qq.com>
2021-05-29 19:16:16 +0800

    dev第一次操作 commit+push

--- src/gittest.java
+++ src/gittest.java
@@ -2,5 +2,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("git第一次操作");
         System.out.println("git的第二次操作");
+        System.out.println("dev分支第一次的操作");
     }
 }
